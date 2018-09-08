b79f47c2a9f3ab61e83d61380caa7a84ae21c982
piteredo <piteredo@hotmail.com>
2018-09-08 20:51:31 +0900

    updt

--- helloworld/Main.java
+++ helloworld/Main.java
@@ -1,12 +1,18 @@
 package java_study.helloworld;
+//package名は小文字のみ、単語間もすべて連結する (java_study も javastudy に)
 
 public class Main {
+	//実行時に引数を渡せる。String の配列で渡ってくる。
+	//引数なくても空の配列を受け取ってるので、String[] args は必須
 	public static void main(String[] args){
+
 		System.out.println("Hello World!");
+		//System.out は PrintStream型 → の println メソッド
+		//System クラスはインスタンス化できない (どのように？)
+
+		System.out.println();
+		//引数なしの println() サポートされてるので純粋な改行に使える ("")のような空文字不要
 	}
-	//実行時は package 名を添える → java java_study.helloworld.Main
-	//実行時に引数を渡せる。Stringの配列で渡ってくる。
-	//System.out は PrintStream型 → の println メソッド
-	//引数なしの println() サポートされてるので純粋な改行に使える ("")不要
-	//package 名に大文字使うとエラーがでる(コンパイルできるからエディタのせい？)
 }
+
+//実行時は class path 以降から書く → java java_study.helloworld.Main
